cf4a8cec1af97480912e28959e645c2abb64fbde
dustyfingers <dustyfingers01257@gmail.com>
2019-07-10 05:18:35 -0600

    brushed up on python syntax of variables and strings, and some string methods

--- strings.py
+++ strings.py
@@ -1,6 +1,88 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
 
-# String Formatting
+name = 'Louie'
+age = 22
 
-# String Methods
+# Concatenation
+print('Hello ' + name +
+      ', you are ' + str(age) +
+      ' years old.')  # cast the int to str
+
+
+# Arguments by position
+print('{}, {}, {}'.format('first', 'second', 'third'))
+print('{1}, {0}, {2}'.format('first', 'second', 'third'))
+
+
+# Arguments by name
+print('Hello my name is {name} and I am {age} years old!'.format(
+    name=name, age=age))
+
+
+# f-strings (python3.6+)
+print(f'Hello my name is {name} and I am {age} years old!')
+
+
+'''
+STRING METHODS
+'''
+s = 'hello There cruel world'
+
+
+# capitalize first letter
+print(s.capitalize())
+
+
+# all uppercase
+print(s.upper())
+
+
+# all lowercase
+print(s.lower())
+
+
+# swapcase
+print(s.swapcase())
+
+
+# get length of str
+print(len(s))
+
+
+# replace series of chars w another
+print(s.replace('world', 'everyone'))
+
+
+# count instances of a char or sequence or chars
+print(s.count('e'))
+
+
+# starts with a char or sequence or chars, returns bool
+print(s.startswith('hello'))
+
+
+# ends with a char or sequence or chars
+print(s.endswith('worldz'))
+
+
+# split into a list
+print(s.split())
+s2 = 'comma, separated, string'
+print(s2.split(', '))
+
+
+# find position of a char or chars
+print(s2.find('r'))
+
+
+# is all alphanumeric
+print(s.isalnum())
+
+
+# is all alphabetic
+print(s.isalpha())
+
+
+# is all numeric
+print(s.isnumeric())
--- variables.py
+++ variables.py
@@ -1,4 +1,5 @@
 # A variable is a container for a value, which can be of various types
+# comments in python are started with hashtags
 
 '''
 This is a 
@@ -13,4 +14,34 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
-print(1)
+
+
+# NO VAR, NO LET NO CONST keyword NO SEMICOLONS in python!
+x = 1  # int
+y = 2.5  # float
+name = 'Lou'  # string
+is_awesome = False  # bool values are UPPERCASE
+many_words_long = 'the language uses snake_case!'  # SNAKE CASE
+
+
+# MULTIPLE ASSIGNMENTS
+var1, var2, var3, is_python_freaking_cool = (1, 2, 'apple!', True)
+
+
+# NO console.log(); instead, print() function
+print(var1, var2, var3)  # returns '1 abc apple!'
+
+
+# basic math
+z = x + y
+print(z)
+
+
+# want to check type?
+print(type(is_awesome))
+
+
+# type casting
+x = str(x)  # 'casts' x as a string
+var2_string = int(var2)  # casts var2 as an int! (you can also cast as float)
+print(var2_string)
