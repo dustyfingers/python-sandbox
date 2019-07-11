102ccb8590b4f58937eb18022cb1a62265aca734
dustyfingers <dustyfingers01257@gmail.com>
2019-07-11 00:12:28 -0600

    learned about python lists, tuples and sets

--- lists.py
+++ lists.py
@@ -1,1 +1,56 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# basically an array
+
+
+# create a list
+# numbers = [1, 2, 3, 4, 5]
+
+
+# using a constructor
+numbers = list((1, 2, 3, 4, 5))
+print(type(numbers))
+
+
+# get value by index
+fruits = ['apples', 'oranges', 'grapes', 'pears']
+print(fruits[0])
+print(fruits[1])
+
+
+# get length
+print(len(fruits))
+
+
+# append to list
+fruits.append('Mangos')
+print(fruits)
+
+
+# remove from list
+fruits.remove('grapes')
+print(fruits)
+
+
+# insert element into position
+fruits.insert(2, 'Strawberries')
+print(fruits)
+
+
+# remove element from position
+fruits.pop(3)
+print(fruits)
+
+
+# reverse list
+fruits.reverse()
+print(fruits)
+
+
+# sort list
+fruits.sort()
+print(fruits)
+
+
+# sort list reverse
+fruits.sort(reverse=True)
+print(fruits)
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,73 @@
+#
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+#
 
 
+#  simple tuple
+fruit_tuple = ('Apple', 'Orange', 'Mango')
+print(fruit_tuple)
+
+
+# tuple using constructor
+fruit_tuple_2 = tuple(('Apple', 'Orange', 'Mango', 'Orange'))
+print(fruit_tuple_2)
+
+
+# get a single value
+print(fruit_tuple[1])
+
+
+# tuple values can not be changed!
+# fruit_tuple[2] = 'Persimmon'  # error!
+
+
+# tuple with one value should have trailing comma! otherwise just returns that value
+fruit_tuple_3 = ('Banana',)
+
+
+# length of tuple
+print(len(fruit_tuple))
+
+
+# delete tuple
+del fruit_tuple_3
+# print(fruit_tuple_3) # error, is not defined
+
+
+#
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+#
+
+
+# simple set
+veggie_set = {'Celery', 'Onion', 'Carrot'}
+print(veggie_set)
+
+
+# no duplicate members in a set!
+# veggie_set = {'Celery', 'Onion', 'Carrot',
+#               'Celery'}  # 2nd celery doesnt show up
+# print(veggie_set)
+
+
+# check if in set
+print('Celery' in veggie_set)
+
+
+# add to set
+veggie_set.add('Broccoli')
+print(veggie_set)
+
+
+# remove from set
+veggie_set.remove('Broccoli')
+print(veggie_set)
+
+
+# clear set completely
+veggie_set.clear()
+print(veggie_set)
+
+
+# delete set
+del veggie_set
